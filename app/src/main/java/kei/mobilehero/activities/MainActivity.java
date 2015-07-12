66d61ba5c4a1f3ada6679f6cfe99c9ba777e0b81
Dimitri75 <bdimitri75@gmail.com>
2015-07-13 00:13:45 +0200

    Add theme song on main screen

--- app/src/main/java/kei/mobilehero/activities/MainActivity.java
+++ app/src/main/java/kei/mobilehero/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package kei.mobilehero.activities;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -14,16 +15,24 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playThemeSong();
     }
 
     public void buttonOnClick(View v) {
-        switch(v.getId()){
+        switch (v.getId()) {
             case R.id.main_activity:
                 Intent i = new Intent(getApplicationContext(), GamesActivity.class);
                 startActivity(i);
                 finish();
                 break;
         }
+    }
+
+    public void playThemeSong(){
+        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.theme);
+        mp.setLooping(true);
+        mp.start();
     }
 
 }
