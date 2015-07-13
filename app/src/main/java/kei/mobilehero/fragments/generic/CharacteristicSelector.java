607f10db55ed9e248abc453c6e9be29c72075507
Vuzi <vuzi@free.fr>
2015-07-13 03:03:59 +0200

    First draft of the new theme for the dialog

--- app/src/main/java/kei/mobilehero/fragments/generic/CharacteristicSelector.java
+++ app/src/main/java/kei/mobilehero/fragments/generic/CharacteristicSelector.java
@@ -2,13 +2,16 @@ package kei.mobilehero.fragments.generic;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.view.LayoutInflater;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import kei.mobilehero.R;
 import kei.mobilehero.classes.attributes.Caracteristic;
 
 /**
@@ -24,6 +27,23 @@ public class CharacteristicSelector {
 
         final CaracteristicListAdapter myAdapter = new CaracteristicListAdapter(characteristics, activity.getLayoutInflater());
 
+        // custom dialog
+        final Dialog dialog = new Dialog(activity, R.style.DialogTheme);
+        dialog.setContentView(R.layout.alertdialog_effect_form);
+        dialog.setTitle(R.string.addEffect);
+
+        Spinner sp = (Spinner) dialog.findViewById(R.id.spinner_alert_effect_characteristics);
+
+        sp.setAdapter(myAdapter);
+
+        dialog.show();
+
+        /*
+
+       // AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+       // builder.set
+
+
         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         builder.setAdapter(myAdapter, new DialogInterface.OnClickListener() {
             @Override
@@ -37,7 +57,7 @@ public class CharacteristicSelector {
             public void onClick(DialogInterface dialog, int which) {}
         });
 
-        builder.show();
+        builder.show();*/
 
     }
 }
