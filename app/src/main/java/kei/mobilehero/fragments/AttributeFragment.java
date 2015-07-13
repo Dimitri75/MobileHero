28cc271f7229716636ded73b937452fda279256f
Dimitri75 <bdimitri75@gmail.com>
2015-07-14 01:12:50 +0200

    Fix seekbar (mana, life)

--- app/src/main/java/kei/mobilehero/fragments/AttributeFragment.java
+++ app/src/main/java/kei/mobilehero/fragments/AttributeFragment.java
@@ -1,7 +1,5 @@
 package kei.mobilehero.fragments;
 
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +9,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -30,13 +29,15 @@ public class AttributeFragment extends FragmentBase implements OnClickListener {
     private Character character;
 
     private EditText nameText;
-    private SeekBar lifeText;
-    private SeekBar manaText;
+    private SeekBar lifeBar;
+    private SeekBar manaBar;
     private EditText genderText;
     private EditText alignmentText;
     private EditText raceText;
     private EditText classNameText;
     private EditText levelText;
+    private TextView manaText;
+    private TextView lifeText;
 
     public AttributeFragment() {
         // Required empty public constructor
@@ -50,21 +51,20 @@ public class AttributeFragment extends FragmentBase implements OnClickListener {
 
         // Instantiate the views
         nameText = (EditText) v.findViewById(R.id.editText_characterName_fragment_attribute);
-        lifeText = (SeekBar) v.findViewById(R.id.seekBar_life_frament_attribute);
-        manaText = (SeekBar) v.findViewById(R.id.seekBar_mana_frament_attribute);
+        lifeBar = (SeekBar) v.findViewById(R.id.seekBar_life_frament_attribute);
+        manaBar = (SeekBar) v.findViewById(R.id.seekBar_mana_frament_attribute);
         genderText = (EditText) v.findViewById(R.id.editText_characterGender_fragment_attribute);
         alignmentText = (EditText) v.findViewById(R.id.editText_characterAlignment_fragment_attribute);
         raceText = (EditText) v.findViewById(R.id.editText_characterRace_fragment_attribute);
         classNameText = (EditText) v.findViewById(R.id.editText_characterClassName_fragment_attribute);
         levelText = (MyCustomEditText) v.findViewById(R.id.editText_characterLevel_fragment_attribute);
+        manaText = (TextView) v.findViewById(R.id.textView_mana);
+        lifeText = (TextView) v.findViewById(R.id.textView_life);
         Button saveButton = (Button) v.findViewById(R.id.button_saveCharacter_fragment_attribute);
 
         saveButton.setOnClickListener(this);
 
-        lifeText.getProgressDrawable().setColorFilter(Color.YELLOW, PorterDuff.Mode.MULTIPLY);
-        lifeText.getThumb().setColorFilter(Color.YELLOW, PorterDuff.Mode.MULTIPLY);
-
-
+        initSeekBars();
 
         getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
@@ -82,10 +82,43 @@ public class AttributeFragment extends FragmentBase implements OnClickListener {
         init();
     }
 
+    public void initSeekBars(){
+
+        lifeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                lifeText.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        manaBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                manaText.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+
     public void init(){
         nameText.setText(character.getName());
-        lifeText.setProgress(character.getLife());
-        manaText.setProgress(character.getMana());
+        lifeBar.setProgress(character.getLife());
+        manaBar.setProgress(character.getMana());
         genderText.setText(character.getGender());
         alignmentText.setText(character.getAlignment());
         raceText.setText(character.getRace());
@@ -101,8 +134,8 @@ public class AttributeFragment extends FragmentBase implements OnClickListener {
                 String characterName;
                 if (!(characterName = nameText.getText().toString()).isEmpty()) {
                     character.setName(characterName);
-                    character.setLife(lifeText.getProgress());
-                    character.setMana(manaText.getProgress());
+                    character.setLife(lifeBar.getProgress());
+                    character.setMana(manaBar.getProgress());
                     character.setGender(genderText.getText().toString());
                     character.setAlignment(alignmentText.getText().toString());
                     character.setRace(raceText.getText().toString());
