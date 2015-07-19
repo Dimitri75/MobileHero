ed8eed744326543415c902659e4aaa1e5445fe12
Vuzi <vuzi@free.fr>
2015-07-19 03:58:17 +0200

    New utils class to download file

---
+++ app/src/main/java/kei/mobilehero/classes/Utils/DownloadImageTask.java
@@ -0,0 +1,40 @@
+package kei.mobilehero.classes.utils;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.InputStream;
+
+/**
+ * Created by Vuzi on 18/07/2015.
+ */
+public abstract class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+
+    public DownloadImageTask() {}
+
+    protected Bitmap doInBackground(String... urls) {
+        String urldisplay = urls[0];
+        Bitmap mIcon11 = null;
+
+        if(urldisplay == null)
+            return mIcon11;
+
+        try {
+            InputStream in = new java.net.URL(urldisplay).openStream();
+            mIcon11 = BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+        return mIcon11;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        onImageReady(result);
+    }
+
+    public abstract void onImageReady(Bitmap result);
+}
